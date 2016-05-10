ec4677d844fc938591cb25f4e32dbddf2b7255a2
xmeta <x_metawork@yahoo.co.jp>
2016-05-10 11:11:25 +0900

    first commit

---
+++ src/Main.hx
@@ -0,0 +1,22 @@
+import electron.main.App;
+import electron.main.BrowserWindow;
+import electron.common.CrashReporter;
+import js.Node.*;
+
+class Main {
+    static function main() {
+        CrashReporter.start({
+            companyName : "hxelectron (not a company)",
+            submitURL : "https://github.com/fponticelli/hxelectron/issues",
+        });
+        App.on( window_all_closed, function() {
+            App.quit();
+        });
+        var mainWindow = null;
+        App.on( ready, function() {
+            mainWindow = new BrowserWindow( { width: 800, height: 600 } );
+            mainWindow.loadURL( 'file://' + __dirname + '/app.html' );
+            mainWindow.on( closed, function() mainWindow = null );
+        });
+    }
+}
